aa074986b434d41b6b9e59030efa3220bb22bb19
gregJohnRob <gregor_r@btinternet.com>
2015-01-12 12:11:10 +0000

    links between pages added

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango says hey there world! \
+                         <br> <a href='/rango/about'>About</a>")
 
 def about(request):
     return HttpResponse("Rango says here is the about page. \
-                           <br> This tutorial has been put together by Gregor Robertson, 2076831")
+                           <br> This tutorial has been put together by Gregor Robertson, 2076831\
+                           <br> <a href='/rango/'>Home</a>")
